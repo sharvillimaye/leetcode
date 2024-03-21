5e31d76eb3f21e01bcc6e475dd308a9524cc61a5
Sharvil Limaye <110076153+sharvillimaye@users.noreply.github.com>
2024-03-21 08:38:59 -0400

    Time: 87 ms (68.21%), Space: 20.7 MB (32.9%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,9 +2,5 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         hashtable = defaultdict(list)
         for string in strs:
-            count = [0] * 26
-            for character in string:
-                count[ord(character) - ord("a")] += 1
-            hashtable[tuple(count)].append(string)
-        
+            hashtable[tuple(sorted(string))].append(string)
         return hashtable.values()
