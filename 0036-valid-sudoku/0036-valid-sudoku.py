0327e6ecd06b00dfc2d360b3fa0a8d21bc1fc9c6
Sharvil Limaye <110076153+sharvillimaye@users.noreply.github.com>
2024-03-23 14:06:17 -0400

    Time: 105 ms (8.63%), Space: 16.6 MB (64.04%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,30 +1,19 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for i in range(len(board)):
-            row = set()
-            for j in range(len(board[0])):
-                if board[i][j] in row:
-                    return False
-                if board[i][j] != ".":
-                    row.add(board[i][j])
+        columns = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)
 
-        for i in range(len(board[0])):
-            column = set()
-            for j in range(len(board)):
-                if board[j][i] in column:
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in columns[c] or 
+                    board[r][c] in squares[(r // 3, c //3)]):
                     return False
-                if board[j][i] != ".":
-                    column.add(board[j][i])
+                rows[r].add(board[r][c])
+                columns[c].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
         
-        for count in range(9):
-            block = set()
-            row_region = (int) (count / 3)
-            column_region = (int) (count % 3)
-            for i in range(0 + (3 * row_region), 3 + (3 * row_region)):
-                for j in range(0 + (3 * column_region), 3 + (3 * column_region)):
-                    if board[i][j] in block:
-                        return False
-                    if board[i][j] != ".":
-                        block.add(board[i][j])
-    
         return True
