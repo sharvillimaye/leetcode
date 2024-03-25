f5994710366535244c73b809947ae99ec88049fe
Sharvil Limaye <110076153+sharvillimaye@users.noreply.github.com>
2024-03-25 19:08:49 -0400

    Time: 49 ms (34.45%), Space: 16.8 MB (99.49%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,8 +1,21 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        string = ""
-        for element in s:
-            if element.isalnum():
-                string += element.lower()
-        reversed_string = string[::-1]
-        return string == reversed_string
+        left_pointer = 0
+        right_pointer = len(s) - 1
+
+        while left_pointer < right_pointer:
+            while not self.alphanum(s[left_pointer]) and left_pointer < len(s) - 1:
+                left_pointer += 1
+            while not self.alphanum(s[right_pointer]) and right_pointer >= 0:
+                right_pointer -= 1
+            
+            if s[left_pointer].lower() != s[right_pointer].lower():
+                return False
+            
+            left_pointer += 1
+            right_pointer -= 1
+            
+        return True
+    
+    def alphanum(self, c):
+        return ord('a') <= ord(c) <= ord('z') or ord('A') <= ord(c) <= ord('Z') or ord('0') <= ord(c) <= ord('9')
