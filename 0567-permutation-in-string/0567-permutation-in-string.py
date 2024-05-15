822e9907ec2c8589ace34aa141ad18532a445e86
Sharvil Limaye <110076153+sharvillimaye@users.noreply.github.com>
2024-05-15 11:47:17 -0400

    Time: 2723 ms (11.7%), Space: 16.6 MB (78.53%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+
+        letters_s1 = {}
+        for letter in s1:
+            letters_s1[letter] = 1 + letters_s1.get(letter, 0)
+        
+        left = 0
+        for right in range(left + len(s1) - 1, len(s2)):
+            while (right - left + 1) > len(s1):
+                left += 1
+            letters_s2 = {}
+            for index in range(left, right + 1):
+                letters_s2[s2[index]] = 1 + letters_s2.get(s2[index], 0)
+            
+            if letters_s2 == letters_s1:
+                return True
+        
+        return False
+
+
+            
