4c251b38cd3a5b85db1157c0ec481bace2ebffd0
Sharvil Limaye <110076153+sharvillimaye@users.noreply.github.com>
2024-06-19 21:20:46 -0700

    Time: 115 ms (60.14%), Space: 17.3 MB (66.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,24 +1,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        left, right = 0, 0
-        letter_map, reference_map = {}, {}
-        min_result = ""
+        if t == "":
+            return ""
 
-        for letter in t:
-            reference_map[letter] = 1 + reference_map.get(letter, 0)
-    
-        for right in range(len(s)):
-            letter_map[s[right]] = 1 + letter_map.get(s[right], 0)
-            while (left <= right and self.checkMaps(reference_map, letter_map)):
-                result = s[left:right+1]
-                if (min_result == "" or right - left + 1 < len(min_result)):
-                    min_result = s[left:right+1]
-                letter_map[s[left]] -= 1
-                left += 1
-        return min_result
-            
-    def checkMaps(self, reference: map, letter: map) -> bool:
-        for key, value in reference.items():
-            if letter.get(key, 0) < value:
-                return False
-        return True
+        countT, window = {}, {}
+        for c in t:
+            countT[c] = 1 + countT.get(c, 0)
+
+        have, need = 0, len(countT)
+        res, resLen = [-1, -1], float("infinity")
+        l = 0
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = 1 + window.get(c, 0)
+
+            if c in countT and window[c] == countT[c]:
+                have += 1
+
+            while have == need:
+                # update our result
+                if (r - l + 1) < resLen:
+                    res = [l, r]
+                    resLen = r - l + 1
+                # pop from the left of our window
+                window[s[l]] -= 1
+                if s[l] in countT and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        l, r = res
+        return s[l : r + 1] if resLen != float("infinity") else ""
