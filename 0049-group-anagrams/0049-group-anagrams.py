10da23385e782fae313d812d554a4972a73d7288
Sharvil Limaye <110076153+sharvillimaye@users.noreply.github.com>
2024-10-03 20:40:19 -0400

    Time: 84 ms (81.46%), Space: 19.4 MB (95.91%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,6 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hashtable = defaultdict(list)
-        for string in strs:
-            hashtable[tuple(sorted(string))].append(string)
-        return hashtable.values()
+        hashmap = {}
+        for element in strs:
+            sorted_element = "".join(sorted(element))
+            if sorted_element in hashmap:
+                hashmap[sorted_element].append(element)
+            else:
+                hashmap[sorted_element] = [element]
+        
+        result = []
+        for key, array in hashmap.items():
+            result.append(array)
+        return result
